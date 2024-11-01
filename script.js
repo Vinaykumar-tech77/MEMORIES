39bbf7a136847c89ad728cd3d982c1fbb7705a81
Vinaykumar-tech77 <127099413+Vinaykumar-tech77@users.noreply.github.com>
2024-11-01 14:45:01 -0230

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('menu');
+    const closeMenu = document.getElementById('close-menu');
 
-    menuToggle.addEventListener('click', () => {
-        if (menu.style.display === 'flex') {
-            menu.style.display = 'none';
-        } else {
-            menu.style.display = 'flex';
-        }
-    });
 
+    const toggleMenu = () => {
+        menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
+    };
+
+    menuToggle.addEventListener('click', toggleMenu);
+    closeMenu.addEventListener('click', toggleMenu);
+
+ 
     document.addEventListener('click', (event) => {
-        if (!menu.contains(event.target) && event.target !== menuToggle) {
+        if (!menu.contains(event.target) && event.target !== menuToggle && event.target !== closeMenu) {
             menu.style.display = 'none';
         }
     });
